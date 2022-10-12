9ec7ba7c6246b081833ff7726a191531bbe745ca
franmassello <64707154+franmassello@users.noreply.github.com>
2022-10-12 10:07:02 -0300

    change bienvenidos to welcome in english

--- src/components/homePageSections/SectionOne/SectionOne.jsx
+++ src/components/homePageSections/SectionOne/SectionOne.jsx
@@ -6,24 +6,23 @@ import { useInView } from "react-intersection-observer";
 import seal from "../../../assets/icons/seal-removebg-preview.png";
 import { AiOutlineArrowDown } from "react-icons/ai";
 import { FiArrowUpRight } from "react-icons/fi";
-import { Trans,useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 
 const SectionOne = () => {
-    const {t, i18n} = useTranslation("homeSectionOne");
-    const [textChange, setTextChange] = useState("")
+    const { t, i18n } = useTranslation("homeSectionOne");
+    const [textChange, setTextChange] = useState("¡Welcome!");
 
-    const lng = useSelector(store => store.changeLngReducer)
+    const lng = useSelector((store) => store.changeLngReducer);
 
     useEffect(() => {
         if (lng === "en") {
-            setTextChange("¡Welcome!")
+            setTextChange("¡Welcome!");
         } else {
-            console.log("super Es ")
-            setTextChange("¡Bienvenidos!")
+            console.log("super Es ");
+            setTextChange("¡Bienvenidos!");
         }
-    }, [textChange])
-
+    }, [textChange]);
 
     const [textStatus, setTextStatus] = useState(0);
 
@@ -41,36 +40,37 @@ const SectionOne = () => {
 
     const refText = useRef();
 
-
     const { text } = useTypewriter({
         words: textStatus === 1 ? [textChange] : null,
         loop: textStatus, // Infinit
     });
 
+    console.log("text", text)
+    console.log("textChange", textChange)
+
     return (
-        <section
-            ref={ref}
-            className={style.contentSectionOne}
-            /* data-aos="zoom-in"
-            data-aos-easing="ease-out-cubic"
-            data-aos-duration="1000" */
-        >
+        <section ref={ref} className={style.contentSectionOne}>
             <div className={style.descriptionContent}>
                 <div className={style.titlesWrip}>
-                <h1 className={style.titleSection} id="textTitle" ref={refText}>
-                    {/* Welcome, to Nutripharco. */}
-                    {text}
-                </h1>
+                    <h1
+                        className={style.titleSection}
+                        id="textTitle"
+                        ref={refText}
+                    >
+                        {text}
+                    </h1>
                 </div>
                 <div className={style.descriptionSection}>
                     {/* <AiOutlineArrowDown />
                     <br />
                     <br />
                     <br /> */}
-                    <p><Trans>{t(`translations.gmp`)}</Trans></p>
+                    <p>
+                        <Trans>{t(`translations.gmp`)}</Trans>
+                    </p>
                     <p>{t(`translations.description`)}</p>
                 </div>
-            </div>  
+            </div>
             <div className={style.imageContent}>
                 <img
                     className={style.logo}
@@ -82,7 +82,7 @@ const SectionOne = () => {
                     data-aos-offset="0"
                     data-aos-duration="200" */
                 />
-                    {/* <Link className={style.learMore} to="/">
+                {/* <Link className={style.learMore} to="/">
                         <i>
                             {t(`translations.more`)} <FiArrowUpRight />
                         </i>
